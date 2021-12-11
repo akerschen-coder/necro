9c835301f5c129572be151e9ddf2932b2826c562
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-12-11 10:32:56 -0700

    first commit- set up

---
+++ server/models/User.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// here we can import for saved stories to user 
+
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    // put in an array of saved stories here when ready
+
+  },
+
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hash user password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+
+const User = model('User', userSchema);
+
+module.exports = User;
